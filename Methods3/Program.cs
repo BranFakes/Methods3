6755f6aef5c2e8a0dfd8ce200549b4c56c59ba12
Brandon VanBuskirk <Brandonvb@hotmail.ca>
2021-09-27 13:24:37 -0300

    Update Program.cs

--- Methods3/Program.cs
+++ Methods3/Program.cs
@@ -8,8 +8,21 @@ namespace Methods3
 {
     class Program
     {
+        static int count = 0; //declare / init
+
+        static void Loop()
+        {
+            count = count + 1;
+            Console.WriteLine("Loopping" + count);
+            Loop(); //Infinate Loop
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Methods 3");
+
+            Loop();
+
+            Console.WriteLine("");
         }
     }
 }
